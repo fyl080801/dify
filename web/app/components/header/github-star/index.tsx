6f7c3b2aa6e90924d8777f099f41367be36375c9
fengyuanliang3 <fengyuanliang2@jd.com>
2025-03-07 18:09:27 +0800

    去掉github

--- web/app/components/header/github-star/index.tsx
+++ web/app/components/header/github-star/index.tsx
@@ -13,38 +13,39 @@ const getStar = async () => {
 }
 
 const GithubStar = () => {
-  const [githubRepo, setGithubRepo] = useState<GithubRepo>({ stargazers_count: 6000 })
-  const [isFetched, setIsFetched] = useState(false)
-  useEffect(() => {
-    (async () => {
-      try {
-        if (process.env.NODE_ENV === 'development')
-          return
+  // const [githubRepo, setGithubRepo] = useState<GithubRepo>({ stargazers_count: 6000 })
+  // const [isFetched, setIsFetched] = useState(false)
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       if (process.env.NODE_ENV === 'development')
+  //         return
 
-        await setGithubRepo(await getStar())
-        setIsFetched(true)
-      }
-      catch (e) {
+  //       await setGithubRepo(await getStar())
+  //       setIsFetched(true)
+  //     }
+  //     catch (e) {
 
-      }
-    })()
-  }, [])
+  //     }
+  //   })()
+  // }, [])
 
-  if (!isFetched)
-    return null
+  // if (!isFetched)
+  //   return null
 
-  return (
-    <a
-      href='https://github.com/langgenius/dify'
-      target='_blank' rel='noopener noreferrer'
-      className='flex items-center leading-[18px] border border-gray-200 rounded-md text-xs text-gray-700 font-semibold overflow-hidden'>
-      <div className='flex items-center px-2 py-1 bg-gray-100'>
-        <Github className='mr-1 w-[18px] h-[18px]' />
-        Star
-      </div>
-      <div className='px-2 py-1 bg-white border-l border-gray-200'>{`${githubRepo.stargazers_count}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</div>
-    </a>
-  )
+  // return (
+  //   <a
+  //     href='https://github.com/langgenius/dify'
+  //     target='_blank' rel='noopener noreferrer'
+  //     className='flex items-center leading-[18px] border border-gray-200 rounded-md text-xs text-gray-700 font-semibold overflow-hidden'>
+  //     <div className='flex items-center px-2 py-1 bg-gray-100'>
+  //       <Github className='mr-1 w-[18px] h-[18px]' />
+  //       Star
+  //     </div>
+  //     <div className='px-2 py-1 bg-white border-l border-gray-200'>{`${githubRepo.stargazers_count}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</div>
+  //   </a>
+  // )
+  return null
 }
 
 export default GithubStar
